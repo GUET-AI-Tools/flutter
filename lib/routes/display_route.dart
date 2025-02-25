c320d0f89c25368d6c5c382a290326634d1001af
YiYue <dyx060312@163.com>
2025-02-25 21:17:39 +0800

    1.优化食材展示界面，提高屏幕利用率 2.修改了登录页面的部分逻辑，现在可以直接跳转到主页 3.表Food新增了type一列

--- lib/routes/display_route.dart
+++ lib/routes/display_route.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DisplayRoute extends StatefulWidget {
+
 
   @override
   State<StatefulWidget> createState() => _DisplayRouteState();
@@ -14,6 +17,16 @@ class _DisplayRouteState extends State<DisplayRoute> {
   late List<Map<String, dynamic>> result;
 
   String username = 'default';
+
+  int? selectedIndex;
+
+  final TextEditingController _numberController = TextEditingController();
+
+  @override
+  void dispose() {
+    _numberController.dispose();
+    super.dispose();
+  }
 
   Future<void> getData() async {
     Database db = await openDatabase(
@@ -30,7 +43,6 @@ class _DisplayRouteState extends State<DisplayRoute> {
 
     _itemCount = result.length;
 
-
     setState(() {
 
     });
@@ -46,6 +58,7 @@ class _DisplayRouteState extends State<DisplayRoute> {
     return Scaffold(
       appBar: AppBar(
         title: Text('还有什么好吃的呢'),
+
       ),
       body: _itemCount == 0
           ? Center(
@@ -57,30 +70,146 @@ class _DisplayRouteState extends State<DisplayRoute> {
         ),
       )
           :
-      ListView.separated(
-        itemCount: _itemCount,
+      foodGridList(),
 
-        itemBuilder: (BuildContext context, int index)  {
+    );
+  }
 
-          String name = result[index]['name'];
-          double number = result[index]['value'] is int
+  Widget foodGridList() { // 网格视图
+    return AnimationLimiter(
+        child: GridView.builder(
+
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1.2
+      ),
+      itemBuilder: (BuildContext context, int index) {
+
+        String name = result[index]['name'];
+        double number = result[index]['value'] is int
             ? (result[index]['value'] as int).toDouble()
             : result[index]['value'];
 
-          // result[index].forEach((n, value) {
-          //   name = n;
-          //   number = value;
-          // });
+        return AnimationConfiguration.staggeredGrid(
+            position: index,
+            columnCount: 3,
+            duration: Duration(milliseconds: 350),
+            child: ScaleAnimation(
+                child: FadeInAnimation(
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Material(
+                        color: Color(0xFFC1F8FF),
+                        borderRadius: BorderRadius.circular(16),
+                        child: InkWell(
 
-          return ListTile(title: Text('$name: ${number.round()}'),);
-        },
+                          onTap: () {
+                            selectedIndex = index;
+                            _numberController.text = number.toInt().toString();
+                            print('点了$index');
+                          },
 
-        separatorBuilder: ((BuildContext context, int index) {
-          return Divider(
-            color: Colors.grey,
-          );
-        }),
-      ),
+                          borderRadius: BorderRadius.circular(16),
+                          splashColor: Color(0xFFC5D3FF),
+
+                          child: Container(
+                            // margin: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+
+                                // border: Border.all(
+                                //     color: Color(0xFFC1F8FF),
+                                //     width: 2
+                                // ),
+                                borderRadius: BorderRadius.circular(16)
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(name,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textScaler: TextScaler.linear(1.3),
+                                ),
+
+                                
+
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    if (selectedIndex == index)
+                                    IconButton(onPressed: () {
+
+                                    },
+                                        icon: Icon(CupertinoIcons.minus_circle)
+                                    ),
+
+                                    if (selectedIndex == index)
+                                      SizedBox(
+                                        width: 25,
+                                        height: 35,
+                                        child: TextField(
+                                          controller: _numberController,
+                                          textAlign: TextAlign.center,
+                                          keyboardType: TextInputType.number,
+
+                                        ),
+                                      )
+                                    else
+                                    Text('${number.round()}',
+                                        textScaler: TextScaler.linear(1.2)
+                                    ),
+
+                                    if (selectedIndex == index)
+                                    IconButton(onPressed: () {
+                                    }, 
+                                        icon: Icon(CupertinoIcons.plus_circle)
+                                    )
+
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                )
+            )
+        );
+      },
+
+      itemCount: _itemCount,
+    )
+    );
+  }
+
+  Widget foodList() { // 列表视图
+
+    return ListView.separated(
+      itemCount: _itemCount,
+
+      itemBuilder: (BuildContext context, int index)  {
+
+        String name = result[index]['name'];
+        double number = result[index]['value'] is int
+            ? (result[index]['value'] as int).toDouble()
+            : result[index]['value'];
+
+        // result[index].forEach((n, value) {
+        //   name = n;
+        //   number = value;
+        // });
+
+        return ListTile(title: Text('$name: ${number.round()}'),);
+      },
+
+      separatorBuilder: ((BuildContext context, int index) {
+        return Divider(
+          color: Colors.grey,
+        );
+      }),
     );
   }
 }
---
+++ lib/routes/login_route.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("登录注册页面",style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.green,
+      ),
+      backgroundColor: Colors.lightGreen[100],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.android,
+                size: 100,
+                color: Colors.green,
+              ),
+              const SizedBox(height: 20),
+              // 应用名
+              const Text(
+                'AI-Tool',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                ),
+              ),
+              const SizedBox(height: 40),
+              // 账号输入文本框
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: '请输入账号',
+                    filled: true,
+                    fillColor: Colors.white70,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              // 密码输入文本框
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: '请输入密码',
+                    filled: true,
+                    fillColor: Colors.white70,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // 处理注册逻辑
+                      ClickedMessage(content: "成功点击注册按钮").show(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 15,
+                      ),
+                    ),
+                    child: const Text(
+                      '注册',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // 处理登录逻辑
+                      ClickedMessage(content: "成功点击登录按钮").show(context);
+
+                      Navigator.pushReplacementNamed(context, 'homepage'); // 暂时先这样
+
+                      // 和这个一样
+                      // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MyHomePage(title: '主页')));
+
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 15,
+                      ),
+                    ),
+                    child: const Text(
+                      '登录',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ClickedMessage{
+  const ClickedMessage({required this.content});
+  final String content;
+
+  void show(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(content),
+        duration: const Duration(seconds: 1),
+      ),
+    );
+  }
+}
